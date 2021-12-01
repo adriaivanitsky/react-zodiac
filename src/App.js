dc5dab93e7f2fda17deaa58b60b3f9576e9161cb
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-11-30 17:46:58 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Main from './components/main/Main';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+
+  )
+}
+
+export default App;
