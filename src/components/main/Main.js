2cb57589f2da421c443fe7cb722590fd30601ff4
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-12-01 18:56:36 -0800

    background img render

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,11 +2,13 @@ import React from 'react'
 import ZodiacCard from '../../zodiac/ZodiacCard'
 import { zodiac } from '../../data'
 import './Main.css'
+import background from '../../../src/nebula.jpeg'
 
 export default function Main() {
     return (
-        <main>
+        <main style={{ backgroundImage: `url(${background })`}}>
             {zodiac.map(sign => <ZodiacCard key={sign.name} name={sign.name} dates={sign.dates} />)}
+
         </main>
     )
 }
